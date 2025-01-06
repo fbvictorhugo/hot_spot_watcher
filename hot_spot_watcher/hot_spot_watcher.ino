c7f6b7bb17a54e3b25dcf6e3ee8028cda7ee2c26
fbvictorhugo <fb.victorhugo@gmail.com>
2025-01-06 20:56:43 -0300

    Base support for OLEDDisplay

--- hot_spot_watcher/hot_spot_watcher.ino
+++ hot_spot_watcher/hot_spot_watcher.ino
@@ -12,6 +12,7 @@
 #include <Wire.h>         // Only needed for Arduino 1.6.5 and earlier
 #include "SSD1306Wire.h"  // legacy include: `#include "SSD1306.h"`
 #include "NodeMcuPins.h"
+#include "OLEDDisplayUi.h"  // Include the UI lib
 
 // Create an instance of the DHT11 class.
 // - For Arduino: Connect the sensor to Digital I/O Pin 2.
@@ -20,6 +21,7 @@
 DHT11 dht11(D4);
 
 SSD1306Wire display(0x3c, D5, D6);
+OLEDDisplayUi ui(&display);
 
 //-------- Variables --------------------------------
 int temperature = 0;
@@ -27,34 +29,92 @@ int humidity = 0;
 const int LED_PIN = D2;
 const int BUZZER_PIN = D1;
 
-const int TEMP_WARNING = 29;
-const int TEMP_CRITICAL = 36;
+const int TEMP_WARNING = 34;
+const int TEMP_CRITICAL = 38;
 const int TONE = 432;
+
+String msgError = "";
+int screenW = 128;
+int screenH = 64;
+
+//-------- UI Displays --------------------------------
+
+void temperatureFrame(OLEDDisplay* display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+
+  if (isDHT11Error()) {
+    display->setTextAlignment(TEXT_ALIGN_CENTER);
+    display->setFont(ArialMT_Plain_16);
+    display->drawString(x + 20, y + 10, "  Erro 2");
+  } else {
+    display->setTextAlignment(TEXT_ALIGN_CENTER);
+    display->setFont(ArialMT_Plain_16);
+    display->drawString(x + 20, y + 10, String(temperature) + " ºC");
+  }
+}
+
+void humidityFrame(OLEDDisplay* display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+
+  if (isDHT11Error()) {
+    display->setTextAlignment(TEXT_ALIGN_CENTER);
+    display->setFont(ArialMT_Plain_16);
+    display->drawString(x + 20, y + 10, " Frame 1");
+  } else {
+    display->setTextAlignment(TEXT_ALIGN_CENTER);
+    display->setFont(ArialMT_Plain_16);
+    display->drawString(x + 20, y + 10, String(humidity) + " %");
+  }
+}
+
+
+void appOverlay(OLEDDisplay* display, OLEDDisplayUiState* state) {
+}
+
+void msOverlay(OLEDDisplay* display, OLEDDisplayUiState* state) {
+  display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(128, 0, String(millis()));
+}
+
+FrameCallback frames[] = { temperatureFrame, humidityFrame };
+OverlayCallback overlays[] = { msOverlay };
 
 //-------- Life Cycle -------------------------------
 
 void setup() {
-  Serial.begin(9600);
+  // pinMode(LED_PIN, OUTPUT);
 
-  display.init();
-  //display.flipScreenVertically();
-  //display.setContrast(255);
-  display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-  display.setFont(ArialMT_Plain_10);
-  writeDisplay("Inicializing ...");
+  Serial.begin(115200);
+  Serial.println();
 
-  display.setFont(ArialMT_Plain_24);
-  pinMode(LED_PIN, OUTPUT);
+
+  ui.setTargetFPS(30);
+  ui.setActiveSymbol(ANIMATION_activeSymbol);
+  ui.setInactiveSymbol(ANIMATION_inactiveSymbol);
+  ui.setIndicatorPosition(BOTTOM);
+  ui.setIndicatorDirection(LEFT_RIGHT);
+  ui.setFrameAnimation(SLIDE_LEFT);
+  ui.setFrames(frames, 2);
+  ui.setOverlays(overlays, 1);
+  ui.init();
 }
 
 void loop() {
 
-  int result = dht11.readTemperatureHumidity(temperature, humidity);
+  int remainingTimeBudget = ui.update();
+
+  if (remainingTimeBudget > 0) {
+    // You can do some work here
+    // Don't do stuff if you are below your
+    // time budget.
+
+    /*
+    int result = dht11.readTemperatureHumidity(temperature, humidity);
 
   if (result == 0) {
+    msgError = "";
+
     ledOff();
     log(temperature, humidity);
-    writeDisplay(String(temperature) + " ºC");
 
     if (temperature >= TEMP_WARNING) {
       ledOn();
@@ -69,7 +129,11 @@ void loop() {
   } else {
     ledOn();
     Serial.println(DHT11::getErrorString(result));
-    writeDisplay("Error " + String(result));
+    msgError = "Error " + String(result);
+  }
+    */
+
+    delay(remainingTimeBudget);
   }
 }
 
@@ -83,11 +147,18 @@ void log(int temperature, int humidity) {
   Serial.println(" %");
 }
 
-void writeDisplay(String text) {
-  display.clear();
-  display.drawString(display.getWidth() / 2, display.getHeight() / 2, text);
-  display.display();
+void writeDisplay(OLEDDisplay* display, String text) {
+  // Limpa a tela para garantir que não haja sobreposição
+  display->clear();
+
+  // Configura o alinhamento e a fonte
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_24);
+
+  // Desenha o texto na posição desejada
+  display->drawString(display->getWidth() / 2, display->getHeight() / 2 - 16, text);
 }
+
 
 void ledOn() {
   digitalWrite(LED_PIN, HIGH);
@@ -100,4 +171,8 @@ void ledOff() {
 void playBuzzer() {
   tone(BUZZER_PIN, TONE, 50);
   Serial.println("BUZZER BUZZER BUZZER");
+}
+
+bool isDHT11Error() {
+  return !msgError.isEmpty();
 }
